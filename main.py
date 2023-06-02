79e7e57141abe2879a8268cbce6f152201679e91
Esmail-Jawabreh <esmailjawabreh@gmail.com>
2023-06-02 17:20:00 +0000

    new changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import eel 
-# from sys import exit
+import eel
+import random
 
 eel.init('web')
 
@@ -7,14 +7,55 @@ rounds = 0
 user_score = 0
 computer_score = 0
 
-username = ""
-userChoise = ""
-computerChoice = ""
+userName = ""
 
 
+@eel.expose
+def setUserName(name):
+    global userName
+    userName = name
+
+    if len(userName) == 0 :
+        userName = "User"
+    else:    
+        userName = name
 
 
+@eel.expose
+def getUserName():
+    global userName
+    return userName
 
 
+@eel.expose
+def setRounds(roundsNumber):
+    global rounds
+    rounds = int(roundsNumber)
+
+@eel.expose
+def getRounds():
+    global rounds
+    return rounds
+
+@eel.expose
+def mouseClassic(userChoise):
+    global user_score, computer_score, rounds
+    
+    choices = ["Rock", "Paper", "Scissors"]
+    computerChoice = random.choice(choices)
+
+    if userChoise == computerChoice:
+        result = "It's a tie!"
+    elif (userChoise == "Rock" and computerChoice == "Scissors") or \
+        (userChoise == "Paper" and computerChoice == "Rock") or \
+        (userChoise == "Scissors" and computerChoice == "Paper"):
+        result = "You win!"
+        user_score += 1
+    else:
+        result = "Computer wins!"
+        computer_score += 1
+
+    return result
 
 eel.start('index.html', port=8080)
+
