b56ac23a84f3255c7430c970b106274de697f7c4
DohaKhamaiseh <dohakhamaiseh22@gmail.com>
2023-06-03 21:15:34 +0300

    Nine Cards Mode done

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import random
 eel.init('web')
 
 rounds = 0
+nine_user_score = 0
+nine_computer_score = 0
+
+nine_user_choice = ""
+nine_computer_choice = ""
+
+userName = ""
+
 user_score = 0
 computer_score = 0
 
-userName = ""
+history = []
+
+rock_count, paper_count, scissors_count = 0, 0, 0
+
+computer_choices = ["Rock", "Paper", "Scissors", "Rock", "Paper", "Scissors", "Rock", "Paper", "Scissors"]
 
 
 @eel.expose
@@ -36,6 +48,7 @@ def setRounds(roundsNumber):
 def getRounds():
     global rounds
     return rounds
+######################################################### mouseClassic ########################
 
 @eel.expose
 def mouseClassic(userChoise):
@@ -57,6 +70,7 @@ def mouseClassic(userChoise):
 
     return result
 
+######################################################### Avatar ########################
 
 @eel.expose
 def Avatar(userChoise):
@@ -77,6 +91,8 @@ def Avatar(userChoise):
         computer_score += 1
 
     return result
+
+######################################################### challenge_mode ########################
 
 @eel.expose
 def challenge_mode(user_choice):
@@ -119,6 +135,86 @@ def coinsChallengeMode():
     global coins
     coinsChallenge = coins
     return coinsChallenge
+
+######################################################### nineCards_mode ########################
+
+# @eel.expose
+# def reset():
+#     global user_score, computer_score, rounds, rock_count, paper_count, scissors_count
+#     user_score = 0
+#     computer_score = 0
+#     rounds = 9
+#     rock_count = 0
+#     paper_count = 0
+#     scissors_count = 0
+
+@eel.expose
+def nineCards_mode(user_choice):
+    global rounds, nine_user_score , nine_computer_score , history, nine_user_choice , nine_computer_choice
+
+    computer_choice = random.choice(computer_choices)
+
+    nine_user_choice = user_choice
+    nine_computer_choice = computer_choice
+
+    if user_choice == computer_choice:
+        result = "It's a tie!"
+    elif (user_choice == "Rock" and computer_choice == "Scissors") or \
+            (user_choice == "Paper" and computer_choice == "Rock") or \
+            (user_choice == "Scissors" and computer_choice == "Paper"):
+        result = "You win!"
+        nine_user_score += 1
+    else:
+        result = "Computer wins!"
+        nine_computer_score += 1
+
+    rounds -= 1
+
+    history.append((rounds + 1, user_choice, computer_choice, nine_user_score, nine_computer_score))
+
+    return result
+
+@eel.expose
+def create_rock_button():
+    global rock_count
+    rock_count += 1
+    return rock_count
+
+@eel.expose
+def create_paper_button():
+    global paper_count
+    paper_count += 1
+    return paper_count
+
+@eel.expose
+def create_scissors_button():
+    global scissors_count
+    scissors_count += 1
+    return scissors_count
+
+
+@eel.expose
+def userScoreArena():
+    global  nine_user_score
+    return  nine_user_score
+   
+
+@eel.expose
+def computerScoreNine():
+    global nine_computer_score
+    return nine_computer_score
+
+# @eel.expose
+# def userChoiceNine():
+#     global nine_user_choice
+#     return nine_user_choice
+
+
+@eel.expose
+def computerChoiceNine():
+    global  nine_computer_choice
+    return  nine_computer_choice
+
 
 eel.start('index.html', port=8080)
 
