b85f697b2b8878177d456d175064c58a0b59f30f
dinajar <deenajaradat2@gmail.com>
2023-06-04 21:02:57 +0300

    reverse

--- web/ReverseMode/startRevers.js
+++ web/ReverseMode/startRevers.js
@@ -63,16 +63,21 @@ async function userChoisePaper() {
     document.getElementById("computerScore").textContent = "Computer Score: " + computerScore;
     document.getElementById("roundCounter").textContent = (rounds - 1) + " Round left";
 
-    if (result === "You win!") {
-        userScore += 1;
-        document.getElementById("userScore").textContent = userName + " Score:" + userScore;
-    } else if (result === "Computer wins!") {
-        computerScore += 1;
-        document.getElementById("computerScore").textContent = "Computer Score: " + computerScore;
-    }
-
     rounds -= 1
     if (rounds == 0) {
+        if (userScore > computerScore){
+            result === "You win!"
+            localStorage.setItem("result", result);
+        }
+        else if (userScore < computerScore){
+            result === "Computer wins!"
+            localStorage.setItem("result", result);
+        }
+        else{
+            result === "It's a tie!"
+            localStorage.setItem("result", result);
+        }
+        
         window.location.href = './endReverse.html'
     }
 }
@@ -89,18 +94,21 @@ async function userChoiseScissor() {
     document.getElementById("computerScore").textContent = "Computer Score: " + computerScore;
     document.getElementById("roundCounter").textContent = (rounds - 1) + " Round left";
 
-    if (result === "You win!") {
-        userScore += 1;
-        document.getElementById("userScore").textContent = userName + " Score:" + userScore;
-    } else if (result === "Computer wins!") {
-
-        computerScore += 1;
-        document.getElementById("computerScore").textContent = "Computer Score: " + computerScore;
-    }
-
     rounds -= 1
     if (rounds == 0) {
-
+        if (userScore > computerScore){
+            result === "You win!"
+            localStorage.setItem("result", result);
+        }
+        else if (userScore < computerScore){
+            result === "Computer wins!"
+            localStorage.setItem("result", result);
+        }
+        else{
+            result === "It's a tie!"
+            localStorage.setItem("result", result);
+        }
+        
         window.location.href = './endReverse.html'
     }
 }
